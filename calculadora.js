950255495f2bddb6e014310134db4cf8d5adbd6c
Luicil Fernandes <luicilfernandes@hotmail.com>
2021-04-12 09:34:37 -0300

    Métodos estáticos

---
+++ calculadora.js
@@ -0,0 +1,17 @@
+class Calculadora{
+
+    static Somar(a, b){
+        var ret = a + b;
+        console.log(ret);
+        return ret;
+    }
+
+    static Sub(a, b){
+        var ret = a - b;
+        console.log(ret);
+        return ret;
+    }
+};
+
+Calculadora.Somar(1,1);
+
